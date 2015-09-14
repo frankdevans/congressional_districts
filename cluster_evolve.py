80d1705533ca8580aa9b4aecdb71d49c0c96afcb
frankdevans <frankdevans@Exaptive-Mac.local>
2015-09-14 09:40:39 -0500

    drop dist_earth customer function in favor of third party package

--- cluster_evolve.py
+++ cluster_evolve.py
@@ -1,9 +1,22 @@
+from haversine import haversine
 
 
-def dist_earth(): pass
+
+
 def init_assignment(): pass
 def evolve_cluster(): pass
 def make_switch_set(): pass
 def make_move_set(): pass
 def cluster_pop_balance(): pass
 def score_cluster(): pass
+
+
+
+
+
+
+
+# Haversine Demo
+loc1 = (35.4826479, -97.4791974)
+loc2 = (35.587409, -97.671185)
+print haversine(loc1, loc2, miles = True)
