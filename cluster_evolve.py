b422e059431b180cdc051bdb14a5af1a0f2156fb
frankdevans <frankdevans@Exaptive-Mac.local>
2015-09-14 09:08:10 -0500

    add Python evolve cluster file with basic interface outline

---
+++ cluster_evolve.py
@@ -0,0 +1,9 @@
+
+
+def dist_earth(): pass
+def init_assignment(): pass
+def evolve_cluster(): pass
+def make_switch_set(): pass
+def make_move_set(): pass
+def cluster_pop_balance(): pass
+def score_cluster(): pass
