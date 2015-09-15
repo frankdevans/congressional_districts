fad56a17795f17b23ac5d73d3eb772a576d7edda
frankdevans <frankdevans@Exaptive-Mac.local>
2015-09-15 09:33:03 -0500

    build, test cluster_pop_balance function

--- cluster_evolve.py
+++ cluster_evolve.py
@@ -39,11 +39,13 @@ for i in raw_data:
 #print zip_coords['73162']
 #print state_zips['OK'][:10]
 
-
 #print haversine(zip_coords['73162'], zip_coords['73106'], miles = True)
 
-
-
+''' Schema Notes
+zip_pop = {zip:pop}
+zip_coords = {zip:(lat,lon)}
+state_zips = {ST:[zip,zip]}
+'''
 
 
 
@@ -51,7 +53,20 @@ def init_assignment(): pass
 def evolve_cluster(): pass
 def make_switch_set(): pass
 def make_move_set(): pass
-def cluster_pop_balance(): pass
+def cluster_pop_balance(cluster, pop_eps):
+    # Sum population for each cluster
+    sum_pop = []
+    for i in cluster:
+        running_k = 0
+        for z in i:
+            running_k += zip_pop[z]
+        sum_pop.append(running_k)
+
+    # Determine if epsilon violations
+    pop_mean = np.mean(sum_pop)
+    pass_high = (np.max(sum_pop) - pop_mean) / pop_mean <= pop_eps
+    pass_low = (pop_mean - np.min(sum_pop)) / pop_mean <= pop_eps
+    return (pass_high and pass_low)
 def score_cluster(cluster):
     collector = []
     for i in cluster:
@@ -75,4 +90,12 @@ print 'Unit Test: score_cluster()'
 start_time = datetime.datetime.now()
 test_k = [['73002', '73003','73004'],['73005','73007','73008','73009']]
 print score_cluster(test_k)
+print 'Execution Time: ', datetime.datetime.now() - start_time
+
+
+print '\n'
+print 'Unit Test: cluster_pop_balance()'
+start_time = datetime.datetime.now()
+test_k = [['73002', '73003','73004'],['73005','73007','73008','73009']]
+print cluster_pop_balance(test_k, 0.1)
 print 'Execution Time: ', datetime.datetime.now() - start_time
